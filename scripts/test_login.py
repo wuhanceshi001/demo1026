eab2aa3be02debefc4d28c7e43208e868997598e
hangzhouceshi02 <hangzhouceshi02@163.com>
2019-10-26 09:28:35 +0800

    第一次提交

---
+++ scripts/test_login.py
@@ -0,0 +1,18 @@
+class TestLogin:
+
+    def test_login_1(self):
+        assert 1
+
+
+    def test_login_2(self):
+        assert 0
+
+    def test_login_3(self):
+        assert 1
+
+
+    def test_login_4(self):
+        assert 0
+
+    def test_login_5(self):
+        assert 1
